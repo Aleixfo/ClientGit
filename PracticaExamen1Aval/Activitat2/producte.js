46103f1f0594c3da594730fa5e4f41e4ee394b50
Aleixfo <afont15182@alumnes.iesmanacor.cat>
2021-12-15 20:39:24 +0100

    Repas activitats examen + activitats JSON

--- PracticaExamen1Aval/Activitat2/producte.js
+++ PracticaExamen1Aval/Activitat2/producte.js
@@ -1,5 +1,7 @@
+//Constructor + parametres esperats
 function Producte(ref, nom, marca, categ, pvp) {
 
+    //Atributs de la classe
     this.ref = ref;
     this.nom = nom;
     this.marca = marca;
@@ -7,12 +9,14 @@ function Producte(ref, nom, marca, categ, pvp) {
     this.pvp = pvp;
 
 
+    //Com fer Metode toString
     this.print = print;
 
     function print() {
         return this.ref + " - " + this.nom + " - " + this.marca + " - " + this.categ + " - " + this.pvp;
     }
 
+    // Descompte
     this.discount = discount;
 
     function discount(quant) {
