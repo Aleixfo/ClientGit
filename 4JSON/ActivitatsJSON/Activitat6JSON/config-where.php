39cbdb703c7b9b72fd5540f0752b53735e48481c
Aleixfo <afont15182@alumnes.iesmanacor.cat>
2021-12-16 21:26:43 +0100

    Actv. JSON i Magatzem

--- 4JSON/ActivitatsJSON/Activitat6JSON/config-where.php
+++ 4JSON/ActivitatsJSON/Activitat6JSON/config-where.php
@@ -8,16 +8,24 @@ Fitxer 1: Taula sense claus externes.
 Fitxer 2: Taula amb 1 o més claus externes.
 */
 
+/* 
+Creació de fitxers JSON a partir del contingut d'una taula de la BBDD. Utilitzar PHP.
+
+Fitxer 1: Taula sense claus externes.
+Fitxer 2: Taula amb 1 o més claus externes.
+*/
+
 
 header("Content-Type: application/json; charset=UTF-8");
 
-$conn = new mysqli("localhost", "admin", "admin", "concesionari");
-$stmt = $conn->prepare("SELECT * FROM carroceria ORDER BY nom");
+$conn = new mysqli("localhost", "root", "admin", "concesionari");
+$stmt = $conn->prepare("SELECT * FROM cotxo WHERE matricula='".$_REQUEST['matricula']."';");
 $stmt->execute();
 $result = $stmt->get_result();
 $outp = $result->fetch_all(MYSQLI_ASSOC);
 
 echo json_encode($outp);
+
 
 
 ?>
